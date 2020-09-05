f0d9ac22c1e61483d4d6a4fecdf047d01bfd8c59
Raman Zatsarenko <rz4983@rit.edu>
2020-09-05 19:18:33 -0400

    read_csv now builds a list of values for each car, each car having 3 features

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -16,24 +16,52 @@ def read_csv(filename):
     :return: array of prices (int values)
     """
 
-    prices = []
+    vehicles = []
 
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
+            current_vehicle = []
             if line_count == 0:
                 print(f'Column names are {", ".join(row)}')
             else:
-                prices.append(row[2])
+                # get year of manufacturing
+                try:
+                    year_of_man = int(row[1])
+                    # current_vehicle.append(int(row[1]))
+                    # current_vehicle.append(int(row[2][1:]))
+                    # current_vehicle.append(int(row[3]))
+                except ValueError:
+                    year_of_man = 0
+                finally:
+                    current_vehicle.append(year_of_man)
+                # get price
+                try:
+                    price = int(row[2][1:])
+                except ValueError:
+                    price = 0
+                finally:
+                    current_vehicle.append(price)
+                # get mileage
+                try:
+                    mileage = int(row[3])
+                except ValueError:
+                    mileage = 0
+                finally:
+                    current_vehicle.append(mileage)
+
+                # append to all vehicles
+                vehicles.append(current_vehicle)
             line_count += 1
-    return prices
+    return vehicles
 
 
 def normalize_data(data):
     """
     assuming the list contains valid data that can be converted from str to int
     convert the python list to numpy array and normalize the data
+    partitions the data into train and test arrays
     :param data: python list of valid str values
     :return: numpy array of normalized values
     """
@@ -43,18 +71,31 @@ def normalize_data(data):
         int_val = int(val[1:])/10000
         # print(int_val)
         normalized_data.append(int_val)
-    arr_normalized_data = numpy.array(normalized_data)
-    # print(normalized_data)
-    return arr_normalized_data
+
+    # 2/3 of all data is used for training
+    train_data = numpy.array(normalized_data[:int(len(normalized_data)*0.66)])
+    # 1/3 of all data is used for testing
+    test_data = numpy.array(normalized_data[int(len(normalized_data)*0.66):])
+
+    # use values computed on train data to normalize train and test data
+    mean = train_data.mean(axis=0)
+    train_data -= mean
+    std = train_data.std(axis=0)
+    train_data /= std
+
+    test_data -= mean
+    test_data /= std
+
+    return train_data, test_data
 
 def main():
     filename = 'vehicle_info.csv'
-    prices = read_csv(filename)
-    print(prices)
-    print(len(prices))
-    normalized_data = normalize_data(prices)
-    print(normalized_data)
-
+    vehicles = read_csv(filename)
+    print(vehicles)
+    print(len(vehicles))
+    # train_data, test_data = normalize_data(prices)
+    # print(len(train_data))
+    # print(len(test_data))
 
 if __name__ == '__main__':
     main()
